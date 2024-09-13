d0d4b517565bd6e21686fa584ab4cf53a7d67457
Meghna Singh <138083187+MeghnaSingh15@users.noreply.github.com>
2024-09-13 20:22:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function volume_sphere() {
 	btn.addEventListener("click", function() {
 		let radius = parseFloat(r.value);
 		if(radius < 0 || isNaN(radius)){
-			v.value = "NaN"
+			v.value = "NaN";
 		} else{
 			let vol = 4/3 * Math.PI * Math.pow(r.value, 3);
 			v.value = v.toFixed(4);
