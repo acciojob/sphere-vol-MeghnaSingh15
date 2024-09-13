3946df90a2853718470fe33e7ff4d66408b89318
Meghna Singh <138083187+MeghnaSingh15@users.noreply.github.com>
2024-09-13 20:21:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function volume_sphere() {
     //Write your code here
-  
+	let r = document.getElementById("radius");
+	let v = document.getElementById("volume");
+	let btn = document.getElementById("submit");
+	btn.addEventListener("click", function() {
+		let radius = parseFloat(r.value);
+		if(radius < 0 || isNaN(radius)){
+			v.value = "NaN"
+		} else{
+			let vol = 4/3 * Math.PI * Math.pow(r.value, 3);
+			v.value = v.toFixed(4);
+		}
+	}); 
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
