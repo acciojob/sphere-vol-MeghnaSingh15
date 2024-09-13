0a4838578893c1fa4218d9b71ae878497ef4b72d
Meghna Singh <138083187+MeghnaSingh15@users.noreply.github.com>
2024-09-13 20:26:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function volume_sphere() {
 			v.value = 'NaN';
 		} else{
 			let vol = 4/3 * Math.PI * Math.pow(r.value, 3);
-			v.value = v.toFixed(4);
+			v.value = vol.toFixed(4);
 		}
 	}); 
 } 
